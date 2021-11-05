22c858e90d8ae0949e41c82c9bc4040acb7b6952
Nathan MacLeod <nathan.a.macleod@gmail.com>
2021-11-05 14:03:49 +0000

    Added some airports to the airports file

---
+++ src/static/js/airports.js
@@ -0,0 +1,14 @@
+let airports = {
+    "London Heathrow Airport": {
+        "longitude": -0.4347049,
+        "latitude": 51.4649581,
+        "runway": "27 L",
+        "name": "London Heathrow Airport"
+    },
+    "Swansea Airport": {
+        "longitude": -4.0633436,
+        "latitude": 51.6090539,
+        "runway": "22",
+        "name": "Swansea Airport"
+    }
+}
