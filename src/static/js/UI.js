a5e06a9cbadb600c01453b30c9b595681d14420e
Nathan MacLeod <nathan.a.macleod@gmail.com>
2021-11-08 11:47:16 +0000

    Improved UX of fog settings

--- src/static/js/UI.js
+++ src/static/js/UI.js
@@ -9,3 +9,14 @@ function updateFog(){
 }
 document.getElementById("fogStart").addEventListener("change", function(){ updateFog() });
 document.getElementById("fogEnd").addEventListener("change", function(){ updateFog() });
+document.getElementById("fogCheckmark").addEventListener("click", function(){
+    if (scene.fog == null){
+        scene.fog = new THREE.Fog(
+            0x79B2D8, 
+            document.getElementById("fogStart").value, 
+            document.getElementById("fogEnd").value
+        );
+    } else {
+        scene.fog = null;
+    }
+});
