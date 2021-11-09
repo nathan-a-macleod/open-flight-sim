f34f2db1538ade4e5528a426bdd7fe351b8015d6
Nathan MacLeod <nathan.a.macleod@gmail.com>
2021-11-09 12:48:32 +0000

    Update screenshot1

--- src/static/js/clouds.js
+++ src/static/js/clouds.js
@@ -48,14 +48,18 @@ function addCloud(params){
 }
 
 let colorsArr = [];
-if (parseInt(weather) > 50 ){
+if (parseInt(weather) <= 25 ){
+    colorsArr.push(0xffffff);
+    colorsArr.push(0xdddddd);
+} else if ((parseInt(weather) > 25) && (parseInt(weather) <= 50)) {
+    colorsArr.push(0xdddddd);
+    colorsArr.push(0xbbbbbb);
+} else if ((parseInt(weather) > 50) && (parseInt(weather) <= 75)){
     colorsArr.push(0x999999);
     colorsArr.push(0x777777);
+} else if ((parseInt(weather) > 75) && (parseInt(weather) <= 100)){
+    colorsArr.push(0x777777);
     colorsArr.push(0x555555);
-} else {
-    colorsArr.push(0xaaaaaa);
-    colorsArr.push(0xdddddd);
-    colorsArr.push(0xffffff);
 }
 
 let cloudLayer1 = addCloud({
@@ -67,7 +71,7 @@ let cloudLayer1 = addCloud({
     cloudPointDistance: 3,
     particleSize: 0.06,
     name: "cloudLayer1",
-    colors: [0xffffff, 0xdddddd, 0xaaaaaa],
+    colors: colorsArr,
     opacity: 0.5 + Math.random() / 4
 });
 
