8e3b20e210f89a3f1487b6805f4af80c04a7baf3
Nathan MacLeod <nathan.a.macleod@gmail.com>
2021-11-10 11:40:21 +0000

    Improve code for tile.js

--- src/static/js/tile.js
+++ src/static/js/tile.js
@@ -2,83 +2,71 @@ let tgeo = new ThreeGeo({
   tokenMapbox: userAPIToken, // <---- set your Mapbox API token here
 });
 
-let activeTile;
-let secondryTile;
-function addTile(radius, resolution, lat, lon){
-// ----------
-// The active tile:
-// ----------
-let activeTerrain = tgeo.getTerrainRgb(
-  [
-    lat,
-    lon
-  ],               // [lat, lng]
-  radius,          // radius of bounding circle (km)
-  resolution       // zoom resolution
-);
+function addTile(radius, resolution, lat, lon, scaleX, scaleY, scaleZ, positionY){
+  // ----------
+  // The active tile:
+  // ----------
+  let activeTerrain = tgeo.getTerrainRgb(
+    [
+      lat,
+      lon
+    ],               // [lat, lng]
+    radius,          // radius of bounding circle (km)
+    resolution       // zoom resolution
+  );
 
-activeTerrain.then(
-  function(value1) {
-    console.log("Active terrain: ", value1);
+  activeTerrain.then(
+    function(value) {
+      console.log(value);
 
-    value1.rotation.x = THREE.Math.degToRad(-90);
-    value1.name = "activeTile";
-    scene.add(value1);
+      value.rotation.x = THREE.Math.degToRad(-90);
 
-    // Add the water
-    /*
-    const geometry = new THREE.PlaneGeometry(1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x57a3c6, side: THREE.DoubleSide});
-    const activeWater = new THREE.Mesh(geometry, material);
-    activeWater.rotation.x = THREE.Math.degToRad(90);
-    activeWater.position.y = 0.00001;
-    
-    scene.add(activeWater);
-    */
-    
-    activeTile = value1;
+      value.scale.x = scaleX;
+      value.scale.y = scaleZ;
+      value.scale.z = scaleY;
 
-    // ----------
-    // The secondry tile:
-    // ----------
-    let secondryTerrain = tgeo.getTerrainRgb(
-      [
-        lat,
-        lon
-      ],            // [lat, lng]
-      100,          // radius of bounding circle (km)
-      10            // zoom resolution
-    );
-    
-    secondryTerrain.then(
-      function(value2) { 
-        console.log("Secondry terrain: ", value2);
-  
-        value2.rotation.x = THREE.Math.degToRad(-90);
-        value2.name = "secondryTile";
-        value2.scale.x = 20;
-        value2.scale.y = 20;
-        value2.scale.z = 1;
-        value2.position.y = -0.05;
-        scene.add(value2);
-        
-        secondryTile = value2;
-  
-        paused = false; // Ensure the game is paused after the first tile is loaded
-      },
+      value.position.y = positionY;
+
+      value.name = "activeTile";
+      scene.add(value);
+
+      // Add the water
+      /*
+      const geometry = new THREE.PlaneGeometry(1, 1);
+      const material = new THREE.MeshBasicMaterial({color: 0x57a3c6, side: THREE.DoubleSide});
+      const activeWater = new THREE.Mesh(geometry, material);
+      activeWater.rotation.x = THREE.Math.degToRad(90);
+      activeWater.position.y = 0.00001;
       
-      function(error) { 
-        alert("A critical error occured. The page will now be refreshed."); 
-        location.reload(); 
-      }
-    );
-  },
-  
-  function(error) { 
-    alert("A critical error occured. The page will now be refreshed."); 
-    location.reload(); 
-  }
-);
+      scene.add(activeWater);
+      */
+    },
+    
+    function(error) { 
+      alert("A critical error occured. The page will now be refreshed."); 
+      location.reload(); 
+    }
+  );
 }
 
-addTile(parseFloat(radius), parseInt(resolution), latCoord, lonCoord);
+addTile(
+  parseFloat(radius), 
+  parseInt(resolution), 
+  parseFloat(latCoord), 
+  parseFloat(lonCoord), 
+  1, 
+  1, 
+  1, 
+  0
+);
+
+addTile(
+  100, 
+  10, 
+  parseFloat(latCoord), 
+  parseFloat(lonCoord), 
+  10, 
+  0, 
+  10, 
+  -0.05
+);
