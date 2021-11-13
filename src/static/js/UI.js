8299f6010a57cd0ce48df47b39231d9b53677b16
Nathan MacLeod <nathan.a.macleod@gmail.com>
2021-11-13 17:47:19 +0000

    Updates

---
+++ src/static/js/UI.js
@@ -0,0 +1,1 @@
+document.getElementById("resetViewBtn").style.display = "block";
--- src/static/js/setup.js
+++ src/static/js/setup.js
@@ -15,6 +15,19 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
     })
 });
 
+document.getElementsByClassName("cesium-viewer-toolbar")[0].remove();
+document.getElementsByClassName("cesium-viewer-timelineContainer")[0].remove();
+document.getElementsByClassName("cesium-viewer-animationContainer")[0].remove();
+document.getElementsByClassName("cesium-viewer-fullscreenContainer")[0].remove();
+
+setTimeout(function(){
+    document.getElementsByClassName("cesium-viewer-bottom")[0].classList.add("UI");
+    document.getElementsByClassName("cesium-viewer-bottom")[0].style.left = null;
+    document.getElementsByClassName("cesium-viewer-bottom")[0].style.bottom = null;
+    document.getElementsByClassName("cesium-viewer-bottom")[0].style.left = "32px";
+    document.getElementsByClassName("cesium-viewer-bottom")[0].style.bottom = "32px";
+}, 5000);
+
 const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
 
 buildingTileset.style = new Cesium.Cesium3DTileStyle({
@@ -25,15 +38,23 @@ buildingTileset.style = new Cesium.Cesium3DTileStyle({
     },
 });
 
-viewer.camera.flyTo({
-    destination : Cesium.Cartesian3.fromDegrees(
-        lonCoord, 
-        latCoord, 
-        400
-    ),
-    orientation : {
-        heading : Cesium.Math.toRadians(heading),
-        pitch : Cesium.Math.toRadians(0.0),
-        roll : 0.0
-    }
+function resetView(){
+    viewer.camera.flyTo({
+        destination : Cesium.Cartesian3.fromDegrees(
+            lonCoord, 
+            latCoord, 
+            400
+        ),
+        orientation : {
+            heading : Cesium.Math.toRadians(heading),
+            pitch : Cesium.Math.toRadians(0.0),
+            roll : 0.0
+        }
+    });
+}
+
+document.getElementById("resetViewBtn").addEventListener("click", function(){
+    resetView();
 });
+
+resetView();
