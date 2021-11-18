e482956bc415c451a634566c756fe4c370e954e0
Nathan MacLeod <nathan.a.macleod@gmail.com>
2021-11-18 09:44:27 +0000

    Improve realism of physics

--- src/static/js/airplane.js
+++ src/static/js/airplane.js
@@ -12,20 +12,24 @@ class Airplane{
     constructor(cruiseSpeed){
         this.cruiseSpeed = cruiseSpeed;
         this.throttle = 75; // Percent
-        this.speed_to_units = 0.03;
-        this.roll = 0;
+        this.speed_to_units = 0.025;
+        this.rollOffset = 0;
     }
 
     update(){
         // Rotate the camera based on user input
-        this.roll += Cesium.Math.toRadians(mousePos.x / 200);
+        this.rollOffset += Cesium.Math.toRadians(mousePos.x / 200);
         camera.setView({
             orientation: {
-                heading : camera.heading + (this.roll / 100),
+                heading : camera.heading + (this.rollOffset / 100),
                 pitch : camera.pitch + Cesium.Math.toRadians(mousePos.y / 250),
                 roll : camera.roll + Cesium.Math.toRadians(mousePos.x / 200)
             }
         });
+
+        // Increase or decrease the speed based on 
+        // whether you're accelerating towards or away from the ground
+        if ((this.speed_to_units > 0.0075) && (this.speed_to_units < 0.04)) this.speed_to_units -= camera.pitch - 0.2 / 25;
 
         camera.moveForward(this.cruiseSpeed * (this.throttle * 0.01) * this.speed_to_units);
     }
